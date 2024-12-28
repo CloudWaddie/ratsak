75579c62ff67f1589c486594eae71bb1139f33c4
CloudWaddie <148834837+CloudWaddie@users.noreply.github.com>
2024-12-28 09:19:26 +0000

    Refactor code to accept process name as command line argument

--- main.c
+++ main.c
@@ -1,25 +1,45 @@
 #include <windows.h>
 #include <aclapi.h>
 #include <stdio.h>
-#include <sddl.h> // Might be able to remove
+#include <sddl.h>
 #include <processthreadsapi.h>
 #include <tlhelp32.h>
 
-const wchar_t* TARGET_PROCESS_NAME = L"explorer";
+
 DWORD pid;
 
 
-int wmain() {
+int wmain(int argc, wchar_t* argv[]) {
+    wchar_t TARGET_PROCESS_NAME[MAX_PATH];
+    if (argc != 2) {
+        wprintf(L"Usage: %ls <process name>\n", argv[0]);
+        return 1;
+    }
+    wcscpy_s(TARGET_PROCESS_NAME, MAX_PATH, argv[1]);
+
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 pe32;
     pe32.dwSize = sizeof(PROCESSENTRY32);
     BOOL success = Process32First(hSnapshot, &pe32);
     if (!success) {
         DWORD error = GetLastError();
-        wprintf(L"FAIL!\n", error);
+        wprintf(L"FAIL! Error: ", error);
         CloseHandle(hSnapshot);
         return 1;
     }
+    if (wcscmp(pe32.szExeFile, TARGET_PROCESS_NAME) == 0) {
+        printf("FOUND PROCESS!");
+    };
+    while (Process32Next(hSnapshot, &pe32)) {
+        if (wcscmp(pe32.szExeFile, TARGET_PROCESS_NAME) == 0) {
+            printf("FOUND PROCESS! PID: %d EXE: %ls\n", pe32.th32ProcessID, pe32.szExeFile);
+            CloseHandle(hSnapshot);
+            return 0;
+        };
+    }
+
+    wprintf(L"Process %ls not found.\n", TARGET_PROCESS_NAME);
     CloseHandle(hSnapshot);
+    
     return 0;
 }
